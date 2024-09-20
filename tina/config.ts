275c6b30a29bc11a7b9f72873cc87005e389c03e
joshbermanssw <joshberman@ssw.com.au>
2024-09-20 17:16:08 +1000

    push space change

--- tina/config.ts
+++ tina/config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
       publicFolder: "public",
     },
   },
+  
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
