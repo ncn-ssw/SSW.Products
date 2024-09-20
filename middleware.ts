3b68d233cd9b088968d0c113b39890f601d5d5be
joshbermanssw <joshberman@ssw.com.au>
2024-09-20 16:30:03 +1000

    differentiate between prod and local development testing

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,27 @@
-// middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const hostname = req.headers.get('host');
-  const url = req.nextUrl;
+  const hostname = req.headers.get('host'); 
+  const url = req.nextUrl; 
 
   console.log('Running in:', process.env.NODE_ENV);
   const isDevelopment = process.env.NODE_ENV === 'development';
 
   let tenant = null;
 
-  if (url.pathname === '/yakshaver') {
-    tenant = 'yakshaver';
-  } else if (url.pathname === '/timepro') {
-    tenant = 'timepro';
+  if (isDevelopment) {
+    if (url.pathname === '/yakshaver') {
+      tenant = 'yakshaver';
+    } else if (url.pathname === '/timepro') {
+      tenant = 'timepro';
+    }
+  } else {
+    if (hostname?.includes('yakshaver.ai')) {
+      tenant = 'yakshaver';
+    } else if (hostname?.includes('timepro.org')) {
+      tenant = 'timepro';
+    }
   }
 
   if (isDevelopment) {
@@ -26,5 +33,6 @@ export function middleware(req: NextRequest) {
   if (tenant) {
     return NextResponse.rewrite(new URL(`/${tenant}`, req.url));
   }
+
   return NextResponse.redirect('https://www.ssw.com.au/404');
 }
