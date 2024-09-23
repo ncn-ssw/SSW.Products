a7a6e35e2eb8121f1b3da8935a3d6d542825e0b5
joshbermanssw <joshberman@ssw.com.au>
2024-09-23 10:58:04 +1000

    add test-logging

--- app/[product]/page.tsx
+++ app/[product]/page.tsx
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation';
 
 export default function ProductPage() {
   const pathname = usePathname(); // Get the current path
+  console.log(pathname);
 
   // Extract the product name from the path, ensuring it's not null
   const product = pathname ? pathname.split('/')[1] : 'Unknown Product';
