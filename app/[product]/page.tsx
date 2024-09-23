a7681bdf7c1013bf1f1a1021eecbb4d4e441d43e
joshbermanssw <joshberman@ssw.com.au>
2024-09-23 15:41:12 +1000

    extrapolate data queerying

--- app/[product]/page.tsx
+++ app/[product]/page.tsx
@@ -1,4 +1,4 @@
-import client from '../../tina/__generated__/client'; // Adjust the path to your generated TinaCMS client
+import client from '../../tina/__generated__/client'; 
 
 interface ProductPageProps {
   params: { product: string };
@@ -6,33 +6,30 @@ interface ProductPageProps {
 
 export default async function ProductPage({ params }: ProductPageProps) {
   const product = params.product;
-
+  
   console.log('Found product:', product);
   console.log(`Querying TinaCMS for ${product}/home.json`);
 
+  const productData = await getPage(product);
+
+  return (
+    <div>
+      <h1>Product: {product}</h1>
+      <pre>{JSON.stringify(productData, null, 2)}</pre>
+    </div>
+  );
+}
+
+
+async function getPage(product: string) {
   try {
     const pageContent = await client.queries.pages({
       relativePath: `${product}/home.json`,
     });
 
-    const productData = pageContent?.data?.pages || {};
-    
-
-    return (
-      <div>
-        <h1>Product: {product}</h1>
-        <pre>{JSON.stringify(productData, null, 2)}</pre>
-      </div>
-    );
+    return pageContent?.data?.pages || {};
   } catch (error) {
     console.error("Error fetching TinaCMS data:", error);
-    return (
-      <div>
-        <h1>Product: {product}</h1>
-        <p>{(error as Error).message}</p>
-        <pre>{JSON.stringify(error, null, 2)}</pre> {/* Log the full error details */}
-        <p>Error fetching data.</p>
-      </div>
-    );
+    throw new Error(`Could not fetch data for ${product}/home.json`);
   }
 }
