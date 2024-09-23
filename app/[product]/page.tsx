94154279967834a43f387e18dd99b1543481dc6c
joshbermanssw <joshberman@ssw.com.au>
2024-09-23 15:20:31 +1000

    logging

--- app/[product]/page.tsx
+++ app/[product]/page.tsx
@@ -15,7 +15,8 @@ export default async function ProductPage({ params }: ProductPageProps) {
       relativePath: `${product}/home.json`,
     });
 
-    const productData = pageContent?.data?.pages || {};
+    // const productData = pageContent?.data?.pages || {};
+    const productData = {};
 
     return (
       <div>
