4fc650bd94c27d5374fa4b504f92050df73db4b2
joshbermanssw <joshberman@ssw.com.au>
2024-09-23 12:06:18 +1000

    bypass to allow for admin pannel

--- middleware.ts
+++ middleware.ts
@@ -1,35 +1,46 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const hostname = request.headers.get('host');
+  const hostname = request.headers.get("host");
   const { pathname } = request.nextUrl;
 
-  const isLocal = hostname?.includes('localhost') || hostname?.includes('127.0.0.1');
+  const isLocal =
+    hostname?.includes("localhost") || hostname?.includes("127.0.0.1");
   let nextResponse;
 
   const productList = process.env.NEXT_PUBLIC_PRODUCT_LIST
     ? JSON.parse(process.env.NEXT_PUBLIC_PRODUCT_LIST)
     : [];
 
+  const pathSegments = pathname
+    .split("/")
+    .filter((segment) => segment.length > 0);
+
+  if (pathname === "/admin/index.html" || pathname.startsWith("/admin/")) {
+    return NextResponse.next();
+  }
+
   if (isLocal) {
-    const pathSegments = pathname.split('/').filter(segment => segment.length > 0);
-    const isProduct = productList.some((product: { product: string; }) => product.product === pathSegments[0]);
+    const isProduct = productList.some(
+      (product: { product: string }) => product.product === pathSegments[0]
+    );
+
     if (isProduct) {
       if (pathSegments.length === 1) {
-    
-        nextResponse = NextResponse.rewrite(new URL(`/${pathSegments[0]}`, request.url));
+        nextResponse = NextResponse.rewrite(
+          new URL(`/${pathSegments[0]}`, request.url)
+        );
       } else if (pathSegments.length === 2) {
-    
-        nextResponse = NextResponse.rewrite(new URL(`/${pathSegments[0]}/${pathSegments[1]}`, request.url));
+        nextResponse = NextResponse.rewrite(
+          new URL(`/${pathSegments[0]}/${pathSegments[1]}`, request.url)
+        );
       }
     } else {
-    
       nextResponse = NextResponse.rewrite(
         new URL(`/${process.env.DEFAULT_PRODUCT}${pathname}`, request.url)
       );
     }
   } else {
-    
     for (const product of productList) {
       if (hostname === product.domain) {
         nextResponse = NextResponse.rewrite(
