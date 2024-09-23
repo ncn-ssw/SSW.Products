72de6f0bd69bfb853099018cef9de952ca6f9045
joshbermanssw <joshberman@ssw.com.au>
2024-09-23 14:53:36 +1000

    push logging

--- app/[product]/page.tsx
+++ app/[product]/page.tsx
@@ -7,12 +7,17 @@ interface ProductPageProps {
 export default async function ProductPage({ params }: ProductPageProps) {
   const product = params.product;
 
+  console.log('Found product: ', product)
+
+  console.log(`Querying TinaCMS for ${product}/home.json`)
+
   try {
     
     const pageContent = await client.queries.pages({
       relativePath: `${product}/home.json`,
     });
 
+  
     
     const productData = pageContent?.data?.pages || {};
 
@@ -23,10 +28,12 @@ export default async function ProductPage({ params }: ProductPageProps) {
       </div>
     );
   } catch (error) {
+    const errorMsg = error
     console.error("Error fetching TinaCMS data:", error);
     return (
       <div>
         <h1>Product: {product}</h1>
+        <p>{(error as Error).message}</p>
         <p>Error fetching data.</p>
       </div>
     );
