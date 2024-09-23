452a7bce25739b1bc5b2322cce2c94af59239c68
joshbermanssw <joshberman@ssw.com.au>
2024-09-23 14:22:31 +1000

    test data wired upto Tina

--- app/[product]/page.tsx
+++ app/[product]/page.tsx
@@ -1,14 +1,35 @@
+import path from 'path';
+import fs from 'fs/promises';
+import client from '../../tina/__generated__/client';
+
 interface ProductPageProps {
   params: { product: string };
 }
 
-export default function ProductPage({ params }: ProductPageProps) {
-  const { product } = params;
+export default async function ProductPage({ params }: ProductPageProps) {
+  const product = params.product;
 
+  
+  const pageContent = await client.queries.pages({
+    relativePath: `${product}/home.json`,
+  });
+
+  
+  const productData = pageContent?.data?.pages || {};
+
+  console.log(productData.title)
+
+  
   return (
     <div>
       <h1>Product: {product}</h1>
-      <p>This is the content for the product: {product}</p>
+      <pre>{JSON.stringify(productData, null, 2)}</pre>
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  const contentDir = path.join(process.cwd(), 'content');
+  const productDirectories = await fs.readdir(contentDir);
+  return productDirectories.map((product) => ({ product }));
 }
