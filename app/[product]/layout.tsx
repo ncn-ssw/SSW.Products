d21a989a97f400f81b35cd6b08668344d93fa069
joshbermanssw <joshberman@ssw.com.au>
2024-09-25 15:42:59 +1000

    fix bg-gradient

--- app/[product]/layout.tsx
+++ app/[product]/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <NavBar />
-      <body className="bg-gradient-to-br from-zinc-900 via-800-700 to-zinc-600 min-h-screen">
+      <body className="bg-gradient-to-br from-stone-800 via-stone-700 to-stone-600 min-h-screen">
         {children}
       </body>
     </html>
