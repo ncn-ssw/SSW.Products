5ac12ba1d0ed06d5f02b4baf492eb126941ac2e7
joshbermanssw <joshberman@ssw.com.au>
2024-09-30 09:19:44 +1000

    rm mock template

--- components/shared/ExampleHero.tsx
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function HeroBlock({ data, index }: { data: { title?: string | null }; index: number }) {
-    return (
-      <div className="text-red-600">{data.title}</div>
-    );
-  }
-  
