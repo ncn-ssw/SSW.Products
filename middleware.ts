4e3e7ed02c3233db93f608575d1a49f22eb7adad
Josh Berman [SSW] <137844305+joshbermanssw@users.noreply.github.com>
2025-01-03 10:28:08 +1100

    another testanother test (#56)

--- middleware.ts
+++ middleware.ts
@@ -7,38 +7,42 @@ export function middleware(request: NextRequest) {
   const isLocal = hostname?.includes('localhost') || hostname?.includes('127.0.0.1');
   const productList = process.env.NEXT_PUBLIC_PRODUCT_LIST ? JSON.parse(process.env.NEXT_PUBLIC_PRODUCT_LIST) : [];
 
-  const pathSegments = pathname.split('/').filter(segment => segment.length > 0);
-
-  // Explicitly allow access to YakShaver's .well-known JSON file
-  if (hostname === 'www.yakshaver.ai' && pathname === '/.well-known/microsoft-identity-association.json') {
-    return NextResponse.next();
+  // Allow .well-known paths without rewriting
+  if (pathname.startsWith('/.well-known')) {
+    return NextResponse.next(); // Bypass rewriting for these paths
   }
 
-  // Allow TinaCMS admin paths to be handled without rewriting
+  // Allow TinaCMS admin paths
   if (pathname.startsWith('/admin')) {
     return NextResponse.next();
   }
 
   if (isLocal) {
-    return handleLocalRequest(pathSegments, productList, request);
+    return handleLocalRequest(pathname, productList, request);
   } else {
     return handleProductionRequest(hostname, productList, pathname, request);
   }
 }
 
-function handleLocalRequest(pathSegments: string[], productList: any[], request: NextRequest) {
+function handleLocalRequest(pathname: string, productList: any[], request: NextRequest) {
+  const pathSegments = pathname.split('/').filter(segment => segment.length > 0);
   const isProduct = productList.some(product => product.product === pathSegments[0]);
 
   if (isProduct) {
     const rewriteUrl = new URL(`/${pathSegments.join('/')}`, request.url);
     return NextResponse.rewrite(rewriteUrl);
   } else {
-    const rewriteUrl = new URL(`/${process.env.DEFAULT_PRODUCT}${request.nextUrl.pathname}`, request.url);
+    const rewriteUrl = new URL(`/${process.env.DEFAULT_PRODUCT}${pathname}`, request.url);
     return NextResponse.rewrite(rewriteUrl);
   }
 }
 
-function handleProductionRequest(hostname: string | null, productList: any[], pathname: string, request: NextRequest) {
+function handleProductionRequest(
+  hostname: string | null,
+  productList: any[],
+  pathname: string,
+  request: NextRequest
+) {
   for (const product of productList) {
     if (hostname === product.domain) {
       const rewriteUrl = new URL(`/${product.product}${pathname}`, request.url);
@@ -48,7 +52,6 @@ function handleProductionRequest(hostname: string | null, productList: any[], pa
   return NextResponse.next();
 }
 
-// Middleware config to exclude static and other paths
 export const config = {
   matcher: [
     '/((?!api|_next|static|favicon.ico|.*\\..*).*)',
