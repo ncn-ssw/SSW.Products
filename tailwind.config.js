0c2d5919f37d01735219c309dfbc6956dc1d4fb7
Caleb Williams [SSW] <65635198+Calinator444@users.noreply.github.com>
2025-04-03 08:16:49 +1100

    Fix styles tailwind cfg (#147)
    * updated tailwind config to look for styles in "./tina"
    
    * uncommented animate module

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     darkMode: ["class"],
     content: [
     "./app/**/*.{js,ts,jsx,tsx}", 
+	"./tina/**/*.{js,ts,jsx,tsx}",
     "./app/*",
     "./components/**/*.{js,ts,jsx,tsx}", 
   ],
