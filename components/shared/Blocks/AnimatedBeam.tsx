8df0523e3afb73c0995cf75bc74e39f601e14b91
Josh Berman [SSW] <137844305+joshbermanssw@users.noreply.github.com>
2025-04-08 16:48:00 +1000

    Make Z Index for Navbar Maximum (#155)
    * Make Z Index for Navbar Maximum
    
    * revert z-index of navbar
    
    * drop z index of animated beam

--- components/shared/Blocks/AnimatedBeam.tsx
+++ components/shared/Blocks/AnimatedBeam.tsx
@@ -21,7 +21,7 @@ const OutputPill = forwardRef<HTMLDivElement, { title: string }>(
         
         <div
           ref={ref}
-          className="relative inline-flex gap-2 py-3 lg:py-2 justify-center items-center md:px-4 px-2 rounded-3xl bg-gradient-to-br from-[#CB4542] to-[#7A2C2A] border border-gray-400 text-xs z-50 text-black"
+          className="relative inline-flex gap-2 py-3 lg:py-2 justify-center items-center md:px-4 px-2 rounded-3xl bg-gradient-to-br from-[#CB4542] to-[#7A2C2A] border border-gray-400 text-xs z-30 text-black"
         >
           <Image
             src={"/svg/github-mark-white.svg"}
@@ -49,7 +49,7 @@ OutputPill.displayName = "OutputPill";
 const CircleLogo = forwardRef<HTMLDivElement, { media: string }>(
   ({ media }, ref) => {
     return (
-      <div className="border border-gray-300 rounded-full z-50" ref={ref}>
+      <div className="border border-gray-300 rounded-full z-30" ref={ref}>
         <div className="md:w-20 m-1 md:h-20 h-16 w-16 rounded-full bg-gray-800 flex items-center justify-center bg-gradient-to-tr from-white via-gray-200 to-gray-500">
           <Image
             src={media || "/svg/yak-icon-fill.svg"}
@@ -71,7 +71,7 @@ const InputBadge = forwardRef<HTMLDivElement, { icon: keyof typeof icons; title:
     const Icon = icons[icon as keyof typeof icons];
 
     return (
-      <div ref={ref} className="inline-flex border border-gray-600 rounded-full z-50">
+      <div ref={ref} className="inline-flex border border-gray-600 rounded-full z-30">
         <div className="relative inline-flex flex-row items-center gap-1 md:gap-2 lg:gap-4 lg:text-sm text-xs rounded-full bg-[#131313] p-1 pr-2 lg:pr-8 border border-gray-600 shadow-[inset_0_0_12px_rgba(156,163,175,0.25)] w-full">
           <div className="bg-[#F8F8F8] bg-opacity-10 rounded-full p-1 lg:p-2">
             {Icon && <Icon />}
