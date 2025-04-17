395a398a80227595eaa95ee3338e0a1fd6b2c6f8
Caleb Williams [SSW] <65635198+Calinator444@users.noreply.github.com>
2025-04-17 14:20:24 +1000

    fixed background using webkit transform (#181)

--- components/shared/Blocks/Hero/GradientBackground.tsx
+++ components/shared/Blocks/Hero/GradientBackground.tsx
@@ -1,9 +1,11 @@
 export function GradientBackground() {
   return (
     <div className="absolute inset-0 z-[-1] pointer-events-none">
+      {/*Webkit Transform fixes for IOS*/}
       <div
         className="absolute w-[100%] h-[50rem] blur-[150px] opacity-[59%] rounded-full left-[-50%] top-[12%] sm:top-[30%] z-[-1] mx-auto"
         style={{
+          WebkitTransform: "translate3d(0, 0, 0);",
           background:
             "linear-gradient(to bottom right, #FF9D3F 0%, #F46772 40%, #AF33E4 69%, black 100%)", //in-line style is the best way to get control of a 40 stop gradient
         }}
